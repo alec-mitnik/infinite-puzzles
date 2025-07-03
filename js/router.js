98f07b21a85f8352384248662fe97318497108bd
alec-mitnik <alecmitnik@gmail.com>
2025-07-03 02:38:41 -0400

    Fixed orientation handling to preserve logical tab order

--- js/router.js
+++ js/router.js
@@ -5,6 +5,9 @@ import { getPuzzleCanvas, stopConfetti, updateForTutorialRecommendation } from '
 /* TODO:
  * Look more into accessibility in general
  * Option to share and recreate a puzzle from a URL?
+ * Keep an updated list of preset seeds to serve as daily challenges
+ * Share times/scores, leaderboard?  Requires emphasis on speed, though...
+ * Keep stats of puzzles completed?
  */
 
 class Router {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-let CACHE_VERSION = 201;
+let CACHE_VERSION = 202;
 // CACHE_VERSION = Date.now();
 const CACHE_NAME = `infinite-puzzles-v${CACHE_VERSION}`;
 
